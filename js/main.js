71609de2c21770542060ad3fe7b1705cb6c06b2b
FilippoBonanno <fbonanno15@gmail.com>
2024-04-30 16:53:23 +0200

    Bonus-1

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@ for (let i = 1; i <= 100; i++) {
     let quadrato = squareCreate(i);
 
     quadrato.addEventListener("click", function() {
-        quadrato.innerText = (i);
+        
         quadrato.classList.toggle("active");
+
+        if (quadrato.classList.contains("active")) {
+            
+            quadrato.innerText = (i);
+        } else {
+            quadrato.innerText = "";
+        }
     });
     
-
     grid.append(quadrato);
     
 }
